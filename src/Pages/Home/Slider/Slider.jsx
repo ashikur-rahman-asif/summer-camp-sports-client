a745fe8997a75c60ad1ce4f3ca0193dab515f377
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-17 15:55:13 +0600

    react-awesome-reveal for bonus marks added

--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -1,26 +1,26 @@
 import Container from "../../../Components/Container/Container";
-
+import { Fade, Slide } from "react-awesome-reveal";
 function Slider() {
-    return(
-        <div>
-            <Container>
-          <div className="carousel w-full mb-10">
+  return (
+    <div>
+  <Container>
+    <div className="carousel w-full mb-10">
       <div id="slide1" className="carousel-item relative w-full">
         <img
           src="https://img.freepik.com/free-photo/different-people-participating-cross-country_23-2149157132.jpg?w=1380&t=st=1686607947~exp=1686608547~hmac=e1c82c7f137326af8cd4fd471bf1c33d5f7a04575f1e4b499427839ec325cc13"
           className="w-full md:h-[90vh]"
         />
-        
+
         <div className="absolute top-1/3 flex flex-col items-start left-20 w-4/6 md:left-1/4 md:w-1/2 text-white">
-          <p className="text-2xl md:text-6xl font-bold">
-          When it comes to summer camp, weird is cool.
-          </p>
+          <Fade direction="up" duration={1000} triggerOnce>
+            <p className="text-2xl md:text-6xl font-bold">
+              When it comes to summer camp, weird is cool.
+            </p>
 
-          <p className=" md:text-xl font-bold mt-2">
-          The end-of-summer winds make people restless.
-          </p>
-
-      
+            <p className="md:text-xl font-bold mt-2">
+              The end-of-summer winds make people restless.
+            </p>
+          </Fade>
         </div>
         <div className="absolute flex justify-between left-5 right-5 bottom-5 md:top-1/2">
           <a href="#slide3" className="text-white md:btn md:btn-circle">
@@ -38,15 +38,15 @@ function Slider() {
         />
         <div className="absolute inset-0 opacity-40 bg-black w-full h-[100vh]"></div>
         <div className="absolute top-1/3 flex flex-col items-start left-20 w-4/6 md:left-1/4 md:w-1/2 text-white">
-          <p className="text-2xl md:text-6xl font-bold">
-          When it comes to summer camp, weird is cool.
-          </p>
+          <Fade direction="up" duration={1000} triggerOnce>
+            <p className="text-2xl md:text-6xl font-bold">
+              When it comes to summer camp, weird is cool.
+            </p>
 
-          <p className=" md:text-xl font-bold mt-2">
-          The end-of-summer winds make people restless.
-          </p>
-
-      
+            <p className="md:text-xl font-bold mt-2">
+              The end-of-summer winds make people restless.
+            </p>
+          </Fade>
         </div>
         <div className="absolute flex justify-between left-5 right-5 bottom-5 md:top-1/2">
           <a href="#slide1" className="text-white md:btn md:btn-circle">
@@ -64,15 +64,15 @@ function Slider() {
         />
         <div className="absolute inset-0 opacity-40 bg-black w-full h-[100vh]"></div>
         <div className="absolute top-1/3 flex flex-col items-start left-20 w-4/6 md:left-1/4 md:w-1/2 text-white">
-          <p className="text-2xl md:text-6xl font-bold">
-          When it comes to summer camp, weird is cool.
-          </p>
+          <Slide direction="left" duration={1000} triggerOnce>
+            <p className="text-2xl md:text-6xl font-bold">
+              When it comes to summer camp, weird is cool.
+            </p>
 
-          <p className=" md:text-xl font-bold mt-2">
-          The end-of-summer winds make people restless.
-          </p>
-
-      
+            <p className="md:text-xl font-bold mt-2">
+              The end-of-summer winds make people restless.
+            </p>
+          </Slide>
         </div>
         <div className="absolute flex justify-between left-5 right-5 bottom-5 md:top-1/2">
           <a href="#slide1" className="text-white md:btn md:btn-circle">
@@ -83,12 +83,10 @@ function Slider() {
           </a>
         </div>
       </div>
-      
     </div>
-    </Container>
-      </div>
-      
-      )
+  </Container>
+</div>
+  );
 }
 
 export default Slider;
